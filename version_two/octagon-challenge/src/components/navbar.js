fe5318ecb2a4840b2659fa504cea867753615266
axel kaban <y>
2017-12-31 12:37:20 -0500

    added nav bar on version two(react), form will be a modal

---
+++ version_two/octagon-challenge/src/components/navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+    render(props){
+        return (
+            <nav>
+                <img className ='logo' src= '#'/>
+                <a href='#'>About us</a>
+                <a href='#'>Product</a>
+                <h2 className = 'contact-button' onClick={()=>{this.props.toggleModal()}}>Contact us</h2>
+            </nav>
+        )
+    }
+}
+export default NavBar;
